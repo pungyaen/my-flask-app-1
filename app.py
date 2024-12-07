56a3e21f1498a30f2821c12f7effe8a9089297be
pungyaen <pungyaen@gmail.com>
2024-12-08 01:27:30 +0700

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,14 @@ app = Flask(__name__)
 def hotel_informaion():
     return render_template('hotel_informaion.html')
 
+@app.route('/status_room')
+def status_room():
+    return render_template('status_room.html')
+
+@app.route('/reservation_form')
+def reservation_form():
+    return render_template('reservation_form.html')
+
 if __name__ == '__main__':
 
     app.run(debug=True)
