9a199b9b7b884d5b6aa14be40197c443305fa708
pungyaen <pungyaen@gmail.com>
2024-12-11 11:27:17 +0700

    Update

--- app.py
+++ app.py
@@ -170,8 +170,8 @@ def create_reservation_image_with_details(reservation_details, slip_path):
 
     try:
         # ฟอนต์
-        font = ImageFont.truetype("arial.ttf", 30)
-        bold_font = ImageFont.truetype("arialbd.ttf", 30)
+        font = ImageFont.truetype("cmss10.ttf.ttf", 30)
+        bold_font = ImageFont.truetype("cmss10.ttf.ttf", 30)
     except IOError:
         font = ImageFont.load_default()
         bold_font = ImageFont.load_default()
